e2fc1238cd2d31c972c678a9a6cd72937ba17cd9
Masaaki Komori <gmgateway@gmail.com>
2025-12-28 07:07:46 +0900

    Modify header

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,37 +5,56 @@ import { SquareCode } from 'lucide-react'
 
 const RootLayout = () => (
   <>
-    <div className="top-0 z-10 sticky flex gap-2 bg-white p-2 border-slate-100 border-b font-light text-xs">
-      <SquareCode className="-mr-1 w-4 h-4" />
-      <Link to="/" className="[&.active]:font-bold [&.active]:text-blue-600">
-        Tanstack Router Home
-      </Link>{' '}
-      <Link
-        to="/playground"
-        className="[&.active]:font-bold [&.active]:text-blue-600"
-      >
-        Playground
-      </Link>
-      <Link
-        to="/button-cn"
-        className="[&.active]:font-bold [&.active]:text-blue-600"
-      >
-        Button w/ cn
-      </Link>
-      <Link
-        to="/button-cva"
-        className="[&.active]:font-bold [&.active]:text-blue-600"
-      >
-        Button w/ cva
-      </Link>
-      <Link
-        to="/card-tv"
-        className="[&.active]:font-bold [&.active]:text-blue-600"
-      >
-        Card w/ tv
-      </Link>
+    <header>
+      <nav className="top-0 z-10 sticky flex gap-2 bg-white p-2 border-slate-100 border-b font-light text-xs">
+        <SquareCode className="-mr-1 w-4 h-4" />
+        <ul className="flex sm:flex-row flex-col gap-2">
+          <li>
+            <Link
+              to="/"
+              className="font-bold [&.active]:font-bold [&.active]:text-blue-600"
+            >
+              Tanstack Router Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/playground"
+              className="[&.active]:font-bold [&.active]:text-blue-600"
+            >
+              Playground
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/button-cn"
+              className="[&.active]:font-bold [&.active]:text-blue-600"
+            >
+              Button w/ cn
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/button-cva"
+              className="[&.active]:font-bold [&.active]:text-blue-600"
+            >
+              Button w/ cva
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/card-tv"
+              className="[&.active]:font-bold [&.active]:text-blue-600"
+            >
+              Card w/ tv
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <div className="p-2">
+      <Outlet />
     </div>
-    <Outlet />
     <TanStackRouterDevtools position="bottom-right" />
   </>
 )
